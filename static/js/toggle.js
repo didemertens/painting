0d01b7fb25b952d7fe9b5a9da96a2f12c602c538
Dide Mertens <d.g.j.mertens@gmail.com>
2019-06-09 13:18:51 +0100

    added three painters for colors

--- static/js/toggle.js
+++ static/js/toggle.js
@@ -1,5 +1,23 @@
-function toggleDarkLight() {
+function toggleMondriaanLight() {
   var body = document.getElementById("body");
   var currentClass = body.className;
-  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+  body.className = currentClass == "mondriaan-mode" ? "light-mode" : "mondriaan-mode";
+}
+
+function toggleRembrandtLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "rembrandt-mode" ? "light-mode" : "rembrandt-mode";
+}
+
+function toggleGoghLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "gogh-mode" ? "light-mode" : "gogh-mode";
+}
+
+function LightMode() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = "light-mode";
 }
