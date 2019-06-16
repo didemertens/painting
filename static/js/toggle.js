155246ca4bdea584a677f1dd48d6a1bbe84f2893
Dide Mertens <d.g.j.mertens@gmail.com>
2019-06-16 16:58:59 +0100

    hidden facts for painters

--- static/js/toggle.js
+++ static/js/toggle.js
@@ -2,15 +2,28 @@ function toggleMondriaanLight() {
   var intro = document.getElementById("intro-text");
   var currentClass = intro.className;
   var factMondriaan = document.getElementById("fact-Mondriaan");
+  var factsPainter = document.getElementsByClassName("facts-painter");
   intro.className = currentClass == "mondriaan-mode" ? "light-mode" : "mondriaan-mode";
+
+  var i;
+  for (i = 0; i < factsPainter.length; i++) {
+    factsPainter[i].style.display = "none";
+  }
   factMondriaan.style.display = "block";
+
 }
 
 function toggleRembrandtLight() {
   var intro = document.getElementById("intro-text");
   var currentClass = intro.className;
   var factRembrandt = document.getElementById("fact-Rembrandt");
+  var factsPainter = document.getElementsByClassName("facts-painter");
   intro.className = currentClass == "rembrandt-mode" ? "light-mode" : "rembrandt-mode";
+
+  var i;
+  for (i = 0; i < factsPainter.length; i++) {
+    factsPainter[i].style.display = "none";
+  }
   factRembrandt.style.display = "block";
 }
 
@@ -18,14 +31,26 @@ function toggleGoghLight() {
   var intro = document.getElementById("intro-text");
   var currentClass = intro.className;
   var factGogh = document.getElementById("fact-Gogh");
+  var factsPainter = document.getElementsByClassName("facts-painter");
   intro.className = currentClass == "gogh-mode" ? "light-mode" : "gogh-mode";
+
+  var i;
+  for (i = 0; i < factsPainter.length; i++) {
+    factsPainter[i].style.display = "none";
+  }
   factGogh.style.display = "block";
 }
 
 function LightMode() {
   var intro = document.getElementById("intro-text");
   var currentClass = intro.className;
+  var factsPainter = document.getElementsByClassName("facts-painter");
   intro.className = "light-mode";
+
+  var i;
+  for (i = 0; i < factsPainter.length; i++) {
+    factsPainter[i].style.display = "none";
+  }
 }
 
 
