7a0f98a7a34315bcc12719aae5d32ec4596e77ab
Dide Mertens <d.g.j.mertens@gmail.com>
2019-06-16 15:32:36 +0100

    only upper part changes, depending on painter

--- static/js/toggle.js
+++ static/js/toggle.js
@@ -1,23 +1,23 @@
 function toggleMondriaanLight() {
-  var body = document.getElementById("body");
-  var currentClass = body.className;
-  body.className = currentClass == "mondriaan-mode" ? "light-mode" : "mondriaan-mode";
+  var intro = document.getElementById("intro-text");
+  var currentClass = intro.className;
+  intro.className = currentClass == "mondriaan-mode" ? "light-mode" : "mondriaan-mode";
 }
 
 function toggleRembrandtLight() {
-  var body = document.getElementById("body");
-  var currentClass = body.className;
-  body.className = currentClass == "rembrandt-mode" ? "light-mode" : "rembrandt-mode";
+  var intro = document.getElementById("intro-text");
+  var currentClass = intro.className;
+  intro.className = currentClass == "rembrandt-mode" ? "light-mode" : "rembrandt-mode";
 }
 
 function toggleGoghLight() {
-  var body = document.getElementById("body");
-  var currentClass = body.className;
-  body.className = currentClass == "gogh-mode" ? "light-mode" : "gogh-mode";
+  var intro = document.getElementById("intro-text");
+  var currentClass = intro.className;
+  intro.className = currentClass == "gogh-mode" ? "light-mode" : "gogh-mode";
 }
 
 function LightMode() {
-  var body = document.getElementById("body");
-  var currentClass = body.className;
-  body.className = "light-mode";
+  var intro = document.getElementById("intro-text");
+  var currentClass = intro.className;
+  intro.className = "light-mode";
 }
