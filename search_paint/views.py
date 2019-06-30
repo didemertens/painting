38f2d4e72b35a9158ca973cdcf2ead830c5a8568
Dide Mertens <d.g.j.mertens@gmail.com>
2019-06-30 19:16:33 +0100

    1 function and html for all paintings

--- search_paint/views.py
+++ search_paint/views.py
@@ -8,8 +8,8 @@ def home(request):
   avercamp = paintings(request, artist_key='SK-A-1718')
   breitner = paintings(request, artist_key='SK-A-3584')
   mauvre = paintings(request, artist_key='SK-A-3602')
-  artists = {'vermeer' : vermeer, 'avercamp' : avercamp, 'breitner' : breitner, 'mauvre' : mauvre}
-  return render(request, 'home.html', artists)
+  artists = [vermeer, avercamp, breitner, mauvre]
+  return render(request, 'home.html', {'artists' : artists})
 
 
 def paintings(request,artist_key):
@@ -33,35 +33,35 @@ def paintingVermeer(request):
         'painting_img' : paint_info['artObject']['webImage']['url'],
     })
 
-def paintingAvercamp(request):
-    API_KEY = settings.API_KEY
-    response = requests.get('https://www.rijksmuseum.nl/api/nl/collection/SK-A-1718?key=' + API_KEY + '&format=json')
-    paint_info = response.json()
-    return render(request, 'painting.html', {
-        'painter': paint_info['artObject']['principalMaker'],
-        'painting_title': paint_info['artObject']['label']['title'],
-        'painting_img' : paint_info['artObject']['webImage']['url'],
-    })
+# def paintingAvercamp(request):
+#     API_KEY = settings.API_KEY
+#     response = requests.get('https://www.rijksmuseum.nl/api/nl/collection/SK-A-1718?key=' + API_KEY + '&format=json')
+#     paint_info = response.json()
+#     return render(request, 'painting.html', {
+#         'painter': paint_info['artObject']['principalMaker'],
+#         'painting_title': paint_info['artObject']['label']['title'],
+#         'painting_img' : paint_info['artObject']['webImage']['url'],
+#     })
 
-def paintingAppel(request):
-    API_KEY = settings.API_KEY
-    response = requests.get('https://www.rijksmuseum.nl/api/nl/collection/SK-A-5002?key=' + API_KEY + '&format=json')
-    paint_info = response.json()
-    return render(request, 'painting.html', {
-        'painter': paint_info['artObject']['principalMaker'],
-        'painting_title': paint_info['artObject']['label']['title'],
-        'painting_img' : paint_info['artObject']['webImage']['url'],
-    })
+# def paintingAppel(request):
+#     API_KEY = settings.API_KEY
+#     response = requests.get('https://www.rijksmuseum.nl/api/nl/collection/SK-A-5002?key=' + API_KEY + '&format=json')
+#     paint_info = response.json()
+#     return render(request, 'painting.html', {
+#         'painter': paint_info['artObject']['principalMaker'],
+#         'painting_title': paint_info['artObject']['label']['title'],
+#         'painting_img' : paint_info['artObject']['webImage']['url'],
+#     })
 
-def paintingMauve(request):
-    API_KEY = settings.API_KEY
-    response = requests.get('https://www.rijksmuseum.nl/api/nl/collection/SK-A-3602?key=' + API_KEY + '&format=json')
-    paint_info = response.json()
-    return render(request, 'painting.html', {
-        'painter': paint_info['artObject']['principalMaker'],
-        'painting_title': paint_info['artObject']['label']['title'],
-        'painting_img' : paint_info['artObject']['webImage']['url'],
-    })
+# def paintingMauve(request):
+#     API_KEY = settings.API_KEY
+#     response = requests.get('https://www.rijksmuseum.nl/api/nl/collection/SK-A-3602?key=' + API_KEY + '&format=json')
+#     paint_info = response.json()
+#     return render(request, 'painting.html', {
+#         'painter': paint_info['artObject']['principalMaker'],
+#         'painting_title': paint_info['artObject']['label']['title'],
+#         'painting_img' : paint_info['artObject']['webImage']['url'],
+#     })
 
 
 # JOHANNES VERMEER
