9f412a0c7aec267c3c835b993f24d4d119d11771
Dide Mertens <d.g.j.mertens@gmail.com>
2019-08-10 11:07:53 +0100

    updated allowed hosts

--- painting/settings.py
+++ painting/settings.py
@@ -28,7 +28,7 @@ API_KEY = os.environ.get('API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
